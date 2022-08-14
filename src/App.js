c9d8c818486191cd07b80679f0c3e7ca27febc08
Gurvinder Singh <chauhanngurvinder15@gmail.com>
2022-08-14 01:47:43 -0400

    Add more functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-//named Imports
-import {Header} from './components/Header';
-import { Footer } from './components/Footer';
-import { Body } from './components/Body';
+// import React from 'react';
 
+//components imported using named Imports
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Body } from "./components/Body";
 
 //function based component syntax:
 export function App() {
-    
-  return (
-      <div className="app-container">
-      {/* passing prop in Header and Footer */}
-      <Header title='Task-Tracker'/>
-      <Body />
-      <Footer footernote ='Year 2022'/>
-      </div>
-  );
+	return (
+		<div className="app-container">
+			{/* passing prop in Header and Footer */}
+			<Header title="Task-Tracker" />
+			<Body />
+			<Footer footernote="Year 2022" />
+		</div>
+	);
 }
